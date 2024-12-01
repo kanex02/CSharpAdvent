d539fe4cd3e5e4f304c3d658b822de9f202b24f3
Kane Xie <kane.xie@taitcommunications.com>
2024-12-02 10:22:10 +1300

    file restructure + first day of 2024

---
+++ 2023/6/Program.cs
@@ -0,0 +1,24 @@
+﻿(long, long)[] inputs = [
+    (48989083, 390110311121360),
+];
+
+long prod = 1;
+
+foreach ((long t, long d) in inputs) {
+    long range = WaysToWin(t, d);
+    System.Console.WriteLine(range);
+    prod *= range;
+}
+
+System.Console.WriteLine(prod);
+
+static long WaysToWin(long time, long recordDistance)
+{
+    double disc = Math.Sqrt(time * time - 4 * recordDistance);
+
+    double max = (time + disc) / 2.0;
+    double min = (time - disc) / 2.0;
+
+    long range = (long)Math.Floor(max) - (long)Math.Floor(min);
+    return range;
+}
---
+++ 2024/1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+StreamReader streamReader = new("./input.txt");
+string? file = streamReader.ReadToEnd();
+
+string[] lines = file.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+
+List<int> list1 = [];
+List<int> list2 = [];
+
+foreach (string line in lines)
+{
+    string[] nums = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    list1.Add(int.Parse(nums[0]));
+    list2.Add(int.Parse(nums[1]));
+}
+
+System.Console.WriteLine(Similarity(list1, list2));
+
+static int Similarity(List<int> list1, List<int> list2)
+{
+    int sum = 0;
+    foreach (int num in list1) {
+        sum += num * list2.Where(i => i == num).Count();
+    }
+    return sum;
+}
+
+static int Distance(List<int> list1, List<int> list2) 
+{
+
+    list1.Sort();
+    list1.Reverse();
+
+    list2.Sort();
+    list2.Reverse();
+
+    int sum = 0;
+    int i = 0;
+    foreach (int num in list1)
+    {
+        sum += Math.Abs(num - list2[i]); 
+        i++;
+    }
+
+    return sum;
+}
+
+
